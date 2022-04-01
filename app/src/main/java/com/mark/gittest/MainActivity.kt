abed985202872fa2c7ea45b4df70abd20043ce7c
liyan <liyan@faw.com.cn>
2022-04-01 22:24:58 +0800

    commit 2

--- app/src/main/java/com/mark/gittest/MainActivity.kt
+++ app/src/main/java/com/mark/gittest/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("TAG","1")
+        Log.d("TAG","2")
     }
 }
