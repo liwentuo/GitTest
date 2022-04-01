b44a6c059f1077a5020b49d873919406a9359732
liyan <liyan@faw.com.cn>
2022-04-01 22:26:02 +0800

    commit 4
    (cherry picked from commit f5ff6663cec9ccef7c101207dd91158d1cbfd64f)

--- app/src/main/java/com/mark/gittest/MainActivity.kt
+++ app/src/main/java/com/mark/gittest/MainActivity.kt
@@ -9,6 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("TAG","5")
-        Log.d("TAG","5")
     }
 }
